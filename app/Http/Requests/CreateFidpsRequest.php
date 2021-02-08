bb5e957e5fd7949f3143336cd140f13dabc9b8be
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:46:54 +0500

    Create CreateFidpsRequest.php

---
+++ app/Http/Requests/CreateFidpsRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateFidpsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'fidp_no' => 'required',
+            'fidp_device_id' => 'required',
+            'device_address' => 'required',
+            'device_status' => 'required',
+            'atollislandsite' => 'required'
+        ];
+    }
+}
