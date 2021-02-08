48b16b8340d47c9bafee80522c6338b161e5c548
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:47:50 +0500

    Create oltInterface.php

---
+++ app/Models/oltInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class oltInterface extends Model
+{
+
+
+    public function olt(){
+
+        return $this->belongsTo ('App\Models\olt','olts_id','id');
+    }
+
+    public function odfinterface(){
+
+        return $this->belongsTo ('App\Models\odfInterface','odf_interfaces_id','id');
+    }
+
+
+}
