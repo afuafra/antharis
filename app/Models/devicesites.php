dd1049d17ecd58395b5a2fb9a084bbdb6c101a47
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:47:00 +0500

    Update devicesites.php

--- app/Models/devicesites.php
+++ app/Models/devicesites.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class devicesites extends Model
 {
-//    public function atollislandsites() {
-//        return $this->AtollCity . ' ' . $this->IslandDistrict. ' ' . $this->Site;
-//    }
+
 }
