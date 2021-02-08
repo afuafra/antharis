b23277a6068b20f42f6931c8711fbbd41678cbd5
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:44:58 +0500

    Create FcabInterfaceController.php

---
+++ app/Http/Controllers/FcabInterfaceController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FcabInterface;
+use Illuminate\Http\Request;
+
+class FcabInterfaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $fcabs = FcabInterface::orderBy("fcab_id","asc")->orderBy("port","asc")->with(['Fcabs','SplitterInterface.FcabSplitter','Splitter','SplitterInterface.FcabSplitter.splitterdevice'])->paginate();
+       return $fcabs;
+
+
+        return view("fcabs_interface.index")->with("fcabs",$fcabs);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
