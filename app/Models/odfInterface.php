7bb2e0774c7bdeb004ed9a2b3ea9c9d71c679f6b
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:47:43 +0500

    Create odfInterface.php

---
+++ app/Models/odfInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class odfInterface extends Model
+{
+    use HasFactory;
+
+    public function odfRack(){
+        return $this->belongsTo ('App\Models\odfRack','odf_racks_id','id');
+
+    }
+
+}
