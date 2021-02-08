a16797c46b43938f0faf36f948caa65b0e043f71
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-09 04:44:54 +0500

    Create FcabController.php

---
+++ app/Http/Controllers/FcabController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\devicesites;
+use App\Models\Fcab;
+use Illuminate\Http\Request;
+
+class FcabController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $fcab=Fcab::orderBy("id","desc")->paginate();
+        $devicesites = devicesites::pluck('atollislandsite')->all();
+
+        return view("fcab.index")->with("fcab_list",$fcab)->with ("devicesites_list",$devicesites) ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $res=new \App\Models\Fcab;
+        $res->fcab_no=$request->input("fcab_no");
+        $res->fcab_device_id=$request->input("fcab_device_id");
+        $res->device_address=$request->input("device_address");
+        $res->device_status=$request->input("device_status");
+        $res->atollislandsite=$request->input("atollislandsite");
+        $res->save();
+
+        $request->session()->flash("msg","New Service Added");
+        return redirect("fcab");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
