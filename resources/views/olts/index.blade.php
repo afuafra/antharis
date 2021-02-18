6a193a3d187c34197fbb86c83802b9e5658952dd
Ibrahim Afrah (AECN) <afrah_i@dhiraagu.com.mv>
2021-02-19 01:30:42 +0500

    olt edit function

--- resources/views/olts/index.blade.php
+++ resources/views/olts/index.blade.php
@@ -147,7 +147,7 @@
                                         {{--            <i class="now-ui-icons ui-2_settings-90"></i>--}}
                                         {{--        </button>--}}
 
-                                        <button type="button" rel="tooltsip" class="btn btn-round btn-round-xs mr4" onclick="editService({{$olt}})">
+                                        <button type="button" rel="tooltsip" class="btn btn-round btn-round-xs mr4" onclick="editOlt({{$olt}})">
                                             <i class="now-ui-icons ui-2_settings-90"></i></button>
 
                                     </td>
@@ -177,11 +177,127 @@
 
     </div>
 
+    <div class="container-fluid">
+        <div class="modal fade" id="editModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Update Service</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form class="container-fluid">
+                        <div class="mb-3">
+                            <input type="hidden" id="_id">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">olt_name</label>
+                            <input type="text" class="form-control" name="_olt_name"
+                                   id="_olt_name">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Device Address</label>
+                            <input type="text" class="form-control" name="_device_address"
+                                   id="_device_address">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Device Status</label>
+                            <input type="text" class="form-control" name="_device_status"
+                                   id="_device_status">
+                        </div>
 
+
+                        <div class="mb-3">
+                            <label class="form-label">Device Site </label>
+                            <select  class="form-control" name="_devicesites_id" list="list" id="_devicesites_id">
+                                @foreach($devicesites_list as $devicesite)
+                                    <option value="{{ $devicesite->id }}">{{ $devicesite->atollislandsite}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">olt_device_id</label>
+                            <input type="text" class="form-control" name="_olt_device_id"
+                                   id="_olt_device_id" readonly>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Back</button>
+                            <button type="button" onclick="updateOlt()" class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
 @endsection
 @push('scripts')
     <script>
+
+        function editOlt(olts){
+            $("#_id").val(olts.id)
+            $("#_olt_name").val(olts.olt_name)
+            $("#_device_address").val(olts.device_address)
+            $("#_device_status").val(olts.device_status)
+            $("#_devicesites_id").val(olts.devicesites_id)
+            $("#_olt_device_id").val(olts.olt_device_id)
+
+            var myModel = new bootstrap.Modal(document.getElementById('editModel'),{
+
+                keyboard: false
+            });
+
+            myModel.show()
+            console.log(olts)
+
+        }
+
+        function updateOlt(olts){
+
+            var id = $("#_id").val()
+            var url = '/olts/'+id
+
+            var formData2 = {
+
+
+
+                'olt_name': $("#_olt_name").val(),
+                'olt_device_id': $("#_device_address").val(),
+                'device_address': $("#_device_status").val(),
+                'device_status': $("#_devicesites_id").val(),
+                'devicesites_id': $("#_olt_device_id").val(),
+                '_token': "{{ csrf_token() }}"
+
+            }
+
+            $.ajax({
+
+                type:"PUT",
+                url: url,
+                data: formData2,
+                dataType: "json",
+
+
+                success: function (data){
+
+                    $("").text('Yey!! OLT Updated')
+                    setTimeout(() => {
+
+                        location.reload()
+
+                    }, 300)
+
+                },
+
+                error: function (error){
+
+                    console.error('ERROR:',error)
+
+                }
+            });
+
+
+
+        }
+
 
         var form = $("#oltsCreate")
         var method = form.attr('method')
